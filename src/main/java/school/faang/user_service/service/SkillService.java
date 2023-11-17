7a18001d7b5dec2bea37aebdd2d76b2359c8a10e
Abramov Nikita <mr.abramovn@gmail.com>
2023-11-17 18:03:28 +0300

    in progress

--- src/main/java/school/faang/user_service/service/SkillService.java
+++ src/main/java/school/faang/user_service/service/SkillService.java
@@ -1,11 +1,13 @@
 package school.faang.user_service.service;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor
 public class SkillService {
 }
